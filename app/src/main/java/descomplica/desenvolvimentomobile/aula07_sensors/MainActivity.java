cc7b62640d2520a6aa5905fd20cc64d24d7721d1
JainyMartins <jainyestefany@gmail.com>
2022-10-26 08:31:07 -0300

    Aula 07 - Sensors

---
+++ app/src/main/java/descomplica/desenvolvimentomobile/aula07_sensors/MainActivity.java
@@ -0,0 +1,14 @@
+package descomplica.desenvolvimentomobile.aula07_sensors;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
